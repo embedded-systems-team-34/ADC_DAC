b988ed25c980ee202e8c359ed70b6ee1954a384c
dmacintyre <djm4912@rit.edu>
2017-04-20 23:24:56 -0400

    Beginning of ADC implementation

--- adc.c
+++ adc.c
@@ -11,6 +11,22 @@
 
 #include "adc.h"
 
+#if DEBUG
+uint8_t buffer[200];
+unsigned int buf_size;
+#endif
+
+struct queue q[16];
+
+// ADC1 Interrupt Handler
+void ADC1_2_IRQHandler() {
+    unsigned int whichInterrupt = ADC1->ISR;
+    // End of sampling flag, set by completion of conversion of any channel at the end of sampling phase
+    if ((whichInterrupt & ADC_ISR_EOSMP) != 0) {
+        
+    }
+}
+
 void adcInit() {
     
     unsigned int i,j;
@@ -41,21 +57,126 @@ void adcInit() {
     
     // Wait for calibration to complete
     while((ADC1->CR & ADC_CR_ADCAL) != 0);
-    ADC1-CR |= ADC_CR_ADEN;
+    ADC1->CR |= ADC_CR_ADEN;
+    
+    adcInitQueues();
+    
+    NVIC_EnableIRQ(ADC1_2_IRQn);
+    // Enable end of sampling flag interrupt
+    ADC1->IER |= ADC_IER_EOSMP;
     
 }
 
-unsigned int selfTest(void) {
+unsigned int adcSelfTest(void) {
+    
+    unsigned int i;
+    unsigned int measured_vref;
+    unsigned int int_mask;
+    
+    // Determine active interrupts so we can mask them back on after selfTest
+    int_mask = ADC1->IER;
+    // Mask of all interupts in selfTest Mode
+    ADC1->IER = 0;
+    
     // Enable the reference voltage
     ADC123_COMMON->CCR |= ADC_CCR_VREFEN;
+    // Give the reference some time to come up and stabalize
+    for (i = 0; i < 10000000; i++) {};
     // Set channel 0, with a channel length of 1
     ADC1->SQR1 = 0;
     // Start a conversion on channel 0
-    ADC1->ADC_CR_ADSTART;
+    ADC1->CR |= ADC_CR_ADSTART;
     // Poll the EOS bit to wait until the conversion is complete
     while ((ADC1->ISR & ADC_ISR_EOS) != ADC_ISR_EOS);
     
     // Verify that VREFEN is as expected
     
+    measured_vref = ADC1->DR;
     
+#if DEBUG
+    buf_size = sprintf((char *)buffer, "VREFEN reading %u\r\n", ADC1->DR);
+    USART_Write(USART2, buffer, buf_size);
+#endif
+    
+    ADC1->IER = int_mask;
+    if (measured_vref > VREF_MIN_THRESHOLD) {
+#if DEBUG
+        buf_size = sprintf((char *)buffer, "Self Test Successful\r\n");
+        USART_Write(USART2, buffer, buf_size);
+#endif
+        return 1;
+    } else {
+#if DEBUG
+        buf_size = sprintf((char *)buffer, "Self Test FAIL\r\n");
+        USART_Write(USART2, buffer, buf_size);
+#endif
+        return 0;
+    }    
 }
+
+void adcInitQueues(void) {
+    
+    unsigned int i = 0;
+    
+    // Initalize the queues, one for each channel
+    for (i = 0; i < NUM_CHANNELS; i++) {
+        init_queue(&q[i]);
+    }
+}
+
+unsigned int isFIFOEmpty(unsigned int channel) {
+    // If valid channel then get number entries of FIFO
+    if (channel < NUM_CHANNELS) {
+        return (isEmpty(&q[channel]));
+    }
+    return 1; 
+}
+
+unsigned int getNumReadingsInFIFO(unsigned int channel) {
+    // If valid channel then get number entries of FIFO
+    if (channel < NUM_CHANNELS) {
+        return (elementsInQueue(&q[channel]));
+    }
+    return 1;
+}
+
+// Set ADC to contionusly convert the currently configured channels 
+void adcSetModeContinous(void) {}
+
+// Set ADC to convert the currently confiugred channels for a single cycle
+void adcSetModeSingle(void) {}
+
+// returns 1 for continous operation, 0 for single
+unsigned int adcGetMode(void) {}
+
+// Perform necessary setup to setup specific channel for conversion
+// chan - channel to enable
+// bit 1 to channel 1 etc.
+void adcInitChannel(unsigned int chan) {
+    // Turn on CLK for Port channel is located on
+    
+    // Close analog switch control for given port
+    
+    // Set sequencing for all channels that are on
+}
+
+// Start a conversion with the currently set mode
+void startConversion(void) {}
+
+// Get most recent adc conversion value for a specified channel samples 
+unsigned int getData(unsigned int channel) {
+    // If valid channel then get number entries of FIFO
+    if (channel < NUM_CHANNELS) {
+        return (pop(&q[channel]));
+    }
+    return 0;    
+}
+
+// Turn on interrupt notification mode
+unsigned int adcInterruptOn(void);
+
+// Turn off interrupt notification mode
+unsigned int adcInterruptsOff(void);
+
+// Set the number of conversion results for which to generate an interrupt
+unsigned int setInterruptWaterline(unsigned int channel, unsigned int waterline);
--- adc.h
+++ adc.h
@@ -8,14 +8,90 @@
 *     Donald MacIntyre - djm4912@rit.edu
 *
 ******************************************************************************/
+#ifndef ADC_H
+#define ADC_H
 
 #define DEBUG (1)
 
+#include "stm32l476xx.h"
+
+#if DEBUG
+#include "UART.h"
+#include <stdio.h>
+#endif
+
+#include "queue.h"
+
+#define VREF_MIN_THRESHOLD (400)
+#define NUM_CHANNELS (16)
+
+/*******************************************************************************
+--------------------------------------------------------------------------------
+ADC1 Channel MAP
+--------------------------------------------------------------------------------
+ADC123_IN0  - VREF - NA 
+ADC123_IN1  - PC0  - MEMS
+ADC123_IN2  - PC1  - MEMS
+ADC123_IN3  - PC2  - MEMS
+ADC123_IN4  - PC3  - MEMS
+ADC123_IN5  - PA0  - JOY_CENTER
+ADC123_IN6  - PA1  - JOY_LEFT
+ADC123_IN7  - PA2  - JOY_RIGHT
+ADC123_IN8  - PA3  - JOY_UP
+ADC123_IN9  - PA4  - 
+ADC123_IN10 - PA5  - JOY_DOWN
+ADC123_IN11 - PA6  - LCD
+ADC123_IN12 - PA7  - LCD
+ADC123_IN13 - PC4  - LCD
+ADC123_IN14 - PC5  - LCD
+ADC123_IN15 - PB0  - LCD
+ADC123_IN16 - PB1  - LCD
+*******************************************************************************/
+
+
 // Measure Vrefint to determine if ADC is operational
 // returns - 1 self test pass, 0 self test fails 
-unsigned int selfTest(void);
+unsigned int adcSelfTest(void);
 
 // Initalization ADC1, turn ADC1 on and perform calibration
-void adcInit();
+void adcInit(void);
 
-void adcConfigureChannel();
+// Setup the FIFOs for data storage on all channels
+void adcInitQueues(void);
+
+// Set ADC to contionusly convert the currently configured channels 
+void adcSetModeContinous(void);
+
+// Set ADC to convert the currently confiugred channels for a single cycle
+void adcSetModeSingle(void);
+
+// returns 1 for continous operation, 0 for single
+unsigned int adcGetMode(void);
+
+// Perform necessary setup to setup specific channel for conversion
+// chan - mask of channels that are enabled where bit 0 corresponds to channel 0, 
+// bit 1 to channel 1 etc.
+void adcInitChannel(unsigned int chan);
+
+// Start a conversion with the currently set mode
+void startConversion(void);
+
+// Get most recent adc conversion value for a specified channel samples 
+unsigned int getData(unsigned int channel);
+
+// Get number of elements waiting to be read from the queue for a specified channel
+unsigned int getNumReadingsInFIFO(unsigned int channel);
+
+// Return 1 iff FIFO is empty, else return 0 if there is at least 1 result to read
+unsigned int isFIFOEmpty(unsigned int channel);
+
+// Turn on interrupt notification mode
+unsigned int adcInterruptOn(void);
+
+// Turn off interrupt notification mode
+unsigned int adcInterruptsOff(void);
+
+// Set the number of conversion results for which to generate an interrupt
+unsigned int setInterruptWaterline(unsigned int channel, unsigned int waterline);
+
+#endif
