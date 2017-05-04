90fd6c44229d8db668804e4053a311a94cd41a07
dmacintyre <djm4912@rit.edu>
2017-05-03 21:28:25 -0400

    Adding demo functionality to functions and allowing dynamic selection

--- main.c
+++ main.c
@@ -13,6 +13,12 @@
 #include <stdlib.h>
 
 #define NUM_ELEMENTS (100)
+#define NUM_DEMOS (14)
+#define DEMO_ENTRIES (NUM_DEMOS + 3)
+
+unsigned int randomNum(unsigned int min, unsigned int max);
+
+uint16_t lut_index = 0;
 
 // 100 Hz sawtooth wave sampled at 10 kHz
 uint16_t sawtooth_lookup_table[NUM_ELEMENTS] = {
@@ -324,78 +330,112 @@ uint16_t cos_lookup_table[NUM_ELEMENTS] = {
 4090,
 4095};
 
-uint16_t flag = 0;
+char help_text[DEMO_ENTRIES][100] = {
+    "**************************************",
+    "0 DAC  - Perform Single Conversion",
+    "1 DAC  - Perform Continous Conversion of cos",
+    "2 DAC  - Perform Continous Conversion of Sawtooth",
+    "3 DAC  - Perform Continous Conversion of Square wave",
+    "4 DAC  - Dynamic FIFO Data source fed with Constant value",
+    "5 DAC  - Dynamic FIFO Data source fed with a triangle wave",
+    "6 DAC  - Dynamic FIFO Data source fed by white noise",
+    "7 DAC  - Dynamic FIFO Data source fed by cos mixed with AWGN noise",
+    "8 ADC  - Test ADC self test mode",
+    "9 ADC  - Test a Single Conversion on a single channel",
+    "10 ADC - Test single conversion on multiple channels",    
+    "11 ADC - Test continous conversion with interrupt notification - single channel", 
+    "12 ADC - Test continous conversion with interrupt notification - multiple channel", 
+    "13 ADC/DAC - Sample the ADC and route the samples to the DAC", 
+    "**************************************",
+    "Enter the demo number to execute followed by the return key: "     
+};
 
-// ADC Waterline Interrupt
-void EXTI0_IRQHandler() {
-    // Clear the interrupt
-    Red_LED_Toggle();
-    flag = 1;
-    // Clear the pending interrupt by writing a 1 to the pending interrupt bit
-    EXTI->PR1 &= EXTI_PR1_PIF0;
+// Prints a help message documenting the commands for the user
+void printDemos() {
+    int        n ;
+    unsigned int i = 0;
+    uint8_t buffer[100];
+    
+    for (i = 0; i < DEMO_ENTRIES; i++) {
+        n = sprintf((char *)buffer, "%s\r\n", help_text[i]);
+        USART_Write(USART2, buffer, n);   
+    }
+    
 }
 
-unsigned int randomNum(unsigned int min, unsigned int max) {
-    return rand() % (max + 1 - min) + min;
-}
-
-uint16_t lut_index = 0;
-
-int main(void){
+void runDacSingleConversion() {
     
-    uint16_t dac_data = 0;
-    
-    int n = 0;
-    uint16_t i = 0;
-    uint8_t buffer[200]; 
+    int n;
+    uint8_t buffer[1000];
 
-	System_Clock_Init(); // Switch System Clock = 80 MHz
-    UART2_Init(); 
-    LED_Init();
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe 1.56V (0x7ff)\r\n");
+    USART_Write(USART2, buffer, n);  
     
-/**********************************************************************************************
-START DAC TEST CODE 
-**********************************************************************************************/	
-// Test Single Mode
-#if 0
-	dacInit(); 
+    dacInit(); 
     setDacSingle();
     setDacActiveChannel(2);
-    singleConversion(0x3ff);
-#endif
-	
-// Test Continous Mode cos
-#if 0
-	dacInit(); 
+    singleConversion(0x7ff);
+}
+
+void runDacContCosConversion() {
+    
+    int n;
+    uint8_t buffer[1000];
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe sinusoidal waveform from 0 to 3.3V sampled at 10 khz with a frequency of 100 Hz\r\n");
+    USART_Write(USART2, buffer, n);  
+    
+    dacInit(); 
     // Select the cosine waveform
     setDacFixedDataSource(cos_lookup_table, NUM_ELEMENTS);
     setDacActiveChannel(2);  
     // Specify a sample rate of 10 khz -> 100 us (2 * 50 us)
     setDacContinous(2);
-#endif
 
-// Test Continous Mode sawtooth
-#if 0
+}
+
+void runDacContSawtoothConversion() {
+    
+    int n;
+    uint8_t buffer[1000];
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe sawtooth waveform from 0 to 3.3V sampled at 10 khz with a frequency of 100 Hz\r\n");
+    USART_Write(USART2, buffer, n);  
+
 	dacInit(); 
     // Select the sawtooth waveform
     setDacFixedDataSource(sawtooth_lookup_table, NUM_ELEMENTS);
     setDacActiveChannel(2);  
     // Specify a sample rate of 10 khz -> 100 us (2 * 50 us)
     setDacContinous(2);
-#endif
 
-// Test Continous Mode square
-#if 0
+}
+
+void runDacContSquareConversion() {
+    
+    int n;
+    uint8_t buffer[1000];
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe square waveform from 0 to 3.3V sampled at 10 khz with a frequency of 100 Hz\r\n");
+    USART_Write(USART2, buffer, n);  
+    
     dacInit(); 
     // Select the square waveform
     setDacFixedDataSource(square_lookup_table, NUM_ELEMENTS);
     setDacActiveChannel(2);  
     // Specify a sample rate of 10 khz -> 100 us (2 * 50 us)
     setDacContinous(2);
-#endif
+}
 
-// Test Dynamic FIFO Constant Source
-#if 0
+void runDacDynamicSourceConstant() {
+    
+    int n;
+    uint8_t buffer[1000];
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe constant value of 3.3V\r\n");
+    n += sprintf((char *)buffer+n, "THIS TEST WILL RUN FOREVER AND NEEDS A MANUAL RESET TO THE STM32 TO RUN THE NEXT TEST\r\n");
+    USART_Write(USART2, buffer, n);  
+    
     // Give the DAC an initial value
     writeDacOutputData(0xfff);
     dacInit(); 
@@ -411,11 +451,20 @@ START DAC TEST CODE
             writeDacOutputData(0xfff);
         }
     }
-#endif
+}
 
-// Test Dynamic FIFO Increasing Source
-// Generate a Triangle Wave at 2.5 Hz as a new sample is output every 100 us and there are 4095 unique samples -> 4095 * 100us roughly 400 ms
-#if 0
+void runDacDynamicSourceTriangleWave() {
+    
+    int n;
+    uint8_t buffer[1000];
+    uint16_t dac_data = 0;
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe triangle wave of 2.5 Hz\r\n");
+    n += sprintf((char *)buffer+n, "THIS TEST WILL RUN FOREVER AND NEEDS A MANUAL RESET TO THE STM32 TO RUN THE NEXT TEST\r\n");
+    USART_Write(USART2, buffer, n);  
+    
+    // Test Dynamic FIFO Increasing Source
+    // Generate a Triangle Wave at 2.5 Hz as a new sample is output every 100 us and there are 4095 unique samples -> 4095 * 100us roughly 400 ms
     // Give the DAC an initial value
     writeDacOutputData(dac_data);
     dacInit(); 
@@ -433,10 +482,20 @@ START DAC TEST CODE
             writeDacOutputData(dac_data);
         }
     }
-#endif
 
+}
+
+void runDacDynamicSourceAWGN() {
+    
+    int n;
+    uint8_t buffer[1000];
+    uint16_t dac_data = 0;
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe AWGN between 0 and 3.3V\r\n");
+    n += sprintf((char *)buffer+n, "THIS TEST WILL RUN FOREVER AND NEEDS A MANUAL RESET TO THE STM32 TO RUN THE NEXT TEST\r\n");
+    USART_Write(USART2, buffer, n);  
+    
 // Test Dynamic FIFO with AWGN Random Noise Source
-#if 0
     // Give the DAC an initial value
     writeDacOutputData(dac_data);
     dacInit(); 
@@ -453,10 +512,19 @@ START DAC TEST CODE
             writeDacOutputData(dac_data);
         }
     }
-#endif
+}
 
+void runDacDynamicSourceCosAWGN() {
+    
+    int n;
+    uint8_t buffer[1000];
+    uint16_t dac_data = 0;
+
+    n = sprintf((char *)buffer, "Measure DAC Channel 2 -> PA5 and observe 100 Hz cos waveform corrupted by AWGN\r\n");
+    n += sprintf((char *)buffer+n, "THIS TEST WILL RUN FOREVER AND NEEDS A MANUAL RESET TO THE STM32 TO RUN THE NEXT TEST\r\n");
+    USART_Write(USART2, buffer, n);  
+    
 // Test Dynamic FIFO with AWGN Random Noise Source mixed with cos waveform
-#if 0
     // Give the DAC an initial value
     writeDacOutputData(dac_data);
     dacInit(); 
@@ -475,15 +543,19 @@ START DAC TEST CODE
             lut_index %= NUM_ELEMENTS;
             writeDacOutputData(dac_data);
         }
-    }
-#endif
+    }  
 
-/**********************************************************************************************
-START ADC TEST CODE 
-**********************************************************************************************/
+}
 
+void runAdcBIST() {
+    
+    int n;
+    uint8_t buffer[1000];
+
+    n = sprintf((char *)buffer, "Run ADC BIST which measures on board reference voltage and validates it is an acceptable value\r\n");
+    USART_Write(USART2, buffer, n);  
+    
 // Test ADC Build In Self Test Mode
-#if 0
     adcInit();
     if (adcSelfTest() == 1) {
         n = sprintf((char *)buffer, "ADC Self Test Successful\r\n");
@@ -492,7 +564,135 @@ START ADC TEST CODE
         n = sprintf((char *)buffer, "ADC Self Test Failed\r\n");
         USART_Write(USART2, buffer, n);         
     }
-#endif
+    
+
+}
+
+// Run the requested demo_index
+void runDemo(unsigned int demo_index) {
+    
+    int    n ;
+    uint8_t buffer[100];
+    
+    switch(demo_index) {
+        /**********************************************************************************************
+        START DAC TEST CODE 
+        **********************************************************************************************/	
+        case 0:
+            runDacSingleConversion();
+            break;
+        case 1:
+            runDacContCosConversion();
+            break;
+        case 2:
+            runDacContSawtoothConversion();
+            break;
+        case 3:
+            runDacContSquareConversion();
+            break;
+        case 4:
+            runDacDynamicSourceConstant();
+            break;
+        case 5:
+            runDacDynamicSourceTriangleWave();
+            break;
+        case 6:
+            runDacDynamicSourceAWGN();
+            break;
+        case 7:
+            runDacDynamicSourceCosAWGN();
+            break;
+        /**********************************************************************************************
+        START ADC TEST CODE 
+        **********************************************************************************************/
+        case 8:
+            runAdcBIST();
+            break;
+        case 9:
+            break;
+        case 10:
+            break;            
+        case 11:
+            break;   
+        case 12:
+            break;   
+        case 13:
+            break;  
+            
+        default:
+            n = sprintf((char *)buffer, "Not a valid entry\r\n");
+            USART_Write(USART2, buffer, n);   
+        
+    }
+}
+
+uint16_t flag = 0;
+unsigned int demo_index = 0;
+
+// ADC Waterline Interrupt
+void EXTI0_IRQHandler() {
+    // Clear the interrupt
+    Red_LED_Toggle();
+    flag = 1;
+    // Clear the pending interrupt by writing a 1 to the pending interrupt bit
+    EXTI->PR1 &= EXTI_PR1_PIF0;
+}
+
+unsigned int randomNum(unsigned int min, unsigned int max) {
+    return rand() % (max + 1 - min) + min;
+}
+
+unsigned int parseUserInput(char *string,unsigned int length) {
+    // Invalid command length so do nothing
+    
+    int n;
+    uint8_t buffer[100];
+    
+    if (length == 0) {
+        return 0;
+    } else {
+        return atoi(string);
+    } 
+    
+}
+
+unsigned int getLine() {
+    
+    char rxByte;
+    int    n ;
+    char userinput[100];
+    unsigned int index = 0;
+    uint8_t buffer[100];
+    
+    n = sprintf((char *)buffer, "> ");
+    USART_Write(USART2, buffer, n);
+    index = 0;
+    // Wait until the user enters a carrige return
+    while(1) {
+        // read the user input and echo it back to the screen
+        rxByte = USART_Read(USART2);    
+        if (rxByte == 0xd) {
+            userinput[index] = '\0';
+            n = sprintf((char *)buffer, "\r\n");
+            USART_Write(USART2, buffer, n);
+            return parseUserInput(userinput, index);
+        }
+        // echo user character back to the screen
+        n = sprintf((char *)buffer, "%s", &rxByte);
+        USART_Write(USART2, buffer, n);    
+        userinput[index++] = (char)rxByte;
+    }    
+}
+
+int main(void){
+    
+    int n = 0;
+    uint16_t i = 0;
+    uint8_t buffer[200]; 
+
+	System_Clock_Init(); // Switch System Clock = 80 MHz
+    UART2_Init(); 
+    LED_Init();
 
 // Test a single conversion on a Single Channel
 #if 0
@@ -605,7 +805,7 @@ JOINT ADC/DAC TEST CODE
 // Set the ADC To sample at 10 kHz on channel PA0
 // Loopback the sampled signal on PA0 to the DAC and output the sampled signal at a rate of 10 kHz
 
-#if 1
+#if 0
     adcInit();
     // Prepare to sample on PA0
     adcInitChannel(5);
@@ -631,6 +831,9 @@ JOINT ADC/DAC TEST CODE
     }
 #endif
     
-	while (1){   
+	while (1) {   
+        printDemos();
+        demo_index = getLine();
+        runDemo(demo_index);
 	}
 }
